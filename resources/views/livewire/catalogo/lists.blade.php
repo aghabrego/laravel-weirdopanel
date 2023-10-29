3691d0a5687a24668481eedec9eba6a550b5307d
Angel Hidalgo <aghabrego@gmail.com>
2023-10-29 13:07:32 -0500

    Fix: Correcting crud design.

--- resources/views/livewire/catalogo/lists.blade.php
+++ resources/views/livewire/catalogo/lists.blade.php
@@ -2,12 +2,12 @@
     <aside class="left-sidebar" data-sidebarbg="skin6">
         <div class="scroll-sidebar" data-sidebarbg="skin6">
             <nav class="sidebar-nav">
-                <ul class="sidebarnav pl-2">
+                <ul class="list-group pl-2">
                     <li class="nav-small-cap"><span class="hide-menu">{{ __('CRUD Menu') }}</span></li>
                     @foreach(\WeirdoPanel\Models\CRUD::active() as $crud)
                         @if (hasPermission(getRouteName().".{$crud->route}.*", $crud->with_acl))
-                            <li class="sidebar-item @isActive(getRouteName().'.'.$crud->route.'.read')">
-                                <a href="@route(getRouteName().'.'.$crud->route.'.read')" class="sidebar-link @isActive(getRouteName().'.'.$crud->route.'.read')">
+                            <li class="list-group-item @isActive(getRouteName().'.'.$crud->route.'.*', 'active')">
+                                <a href="@route(getRouteName().'.'.$crud->route.'.read')" class="sidebar-link @isActive(getRouteName().'.'.$crud->route.'.*', 'text-white')" aria-expanded="false">
                                     <span class="hide-menu">{{ __(\Illuminate\Support\Str::plural(ucfirst($crud->name))) }}</span>
                                 </a>
                             </li>
