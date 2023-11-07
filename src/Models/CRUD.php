166c5417f5bb7aaec88221a5471281b75739e432
Angel Hidalgo <aghabrego@gmail.com>
2023-11-07 17:16:51 -0500

    Fix: Correcting multiple connections error.

--- src/Models/CRUD.php
+++ src/Models/CRUD.php
@@ -3,11 +3,34 @@
 namespace WeirdoPanel\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use WeirdoPanel\Traits\CustomConnection;
 
 class CRUD extends Model
 {
+    use CustomConnection;
+
+    /**
+     * @var string
+     */
     protected $table = 'cruds';
+
+    /**
+     * @var array
+     */
     protected $guarded = [];
+
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setDefaultConnection();
+    }
 
     public function scopeActive($query)
     {
---
+++ src/Traits/CustomConnection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WeirdoPanel\Traits;
+
+trait CustomConnection
+{
+    /**
+     * @return void
+     */
+    public function setDefaultConnection(): void
+    {
+        $db = env('DB_DATABASE', null);
+
+        if (!empty($db)) {
+            \Illuminate\Support\Facades\DB::connection()->useDatabase($db);
+        }
+    }
+}
